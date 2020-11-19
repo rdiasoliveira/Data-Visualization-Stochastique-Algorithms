9ccf3a3ed94a0e3d8920f110b402b53adb86a317
dias-olr <rdiasoli97@gmail.com>
2020-11-19 20:06:38 +0000

    Creating root component which is now using the menu and settings component. Also adding some minor changes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Menu from './components/menu/Menu'
+import Settings from './components/dialog/Settings'
+import Option from './components/dialog/Option'
+import SettingsOptions from './datasource/SettingsOptions'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+	const settingsOptions = SettingsOptions.data;
+
+	const [isSettingsDialogActive, setSettingsDialogActive] = React.useState(false);
+	const [isOptionDialogActive, setOptionDialogActive] = React.useState(false);
+	const [optionDialogContent, setOptionDialogContent] = React.useState(null);
+
+	const handleSetSettingsDialogActive = () => {
+		setSettingsDialogActive(true);
+	};
+
+	const handleSetSettingsDialogInactive = () => {
+		setSettingsDialogActive(false);
+	};
+
+	const handleSetOptionDialogActive = (option) => {
+		setOptionDialogContent(option);
+		setOptionDialogActive(true);
+	}
+
+	const handleSetOptionDialogInactive = () => {
+		setOptionDialogActive(false);
+	}
+
+	React.useEffect(() => {
+		fetch("http://127.0.0.1:8080/1")
+			.then(res => res.json())
+			.then(
+				(result) => {
+					console.log(result);
+				},
+				// Remarque : il est important de traiter les erreurs ici
+				// au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+				// des exceptions provenant de réels bugs du composant.
+				(error) => {
+					console.log(error);
+				}
+			)
+	}, []);
+
+	return (
+		<Menu setSettingsDialogActive={handleSetSettingsDialogActive}>
+			<Settings isSettingsDialogActive={isSettingsDialogActive}
+				setSettingsDialogInactive={handleSetSettingsDialogInactive}
+				setOptionDialogActive={handleSetOptionDialogActive}
+				settingsOptions={settingsOptions} />
+			<Option isOptionDialogActive={isOptionDialogActive}
+				setOptionDialogInactive={handleSetOptionDialogInactive}
+				optionDialogContent={optionDialogContent} />
+		</Menu>
+	);
 }
-
-export default App;
